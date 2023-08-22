0f490b6a8ca897986bce77ae67de765f97401bd1
DejanMilosevic <milosevicdejan65@gmail.com>
2023-08-22 22:32:29 +0200

    kreiran i povezan model

--- rentacarback/app/Models/Car.php
+++ rentacarback/app/Models/Car.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Car extends Model
 {
     use HasFactory;
+       protected $fillable = [
+        'brand_id',
+        'model',
+        'year',
+        'price_per_day',
+        'is_available',
+    ];
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function rentals()
+    {
+        return $this->hasMany(Rental::class);
+    }
 }
