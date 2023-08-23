2805aa56ed6dd593bd091025ed5136f14c05c460
DejanMilosevic <milosevicdejan65@gmail.com>
2023-08-23 22:37:30 +0200

    pretraga po modelu i brendu

--- src/Ponuda.jsx
+++ src/Ponuda.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CarCard from './CarCard';
- 
 
 const Ponuda = ({ cars }) => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [displayCars, setDisplayCars] = useState(cars); // Ovo je niz automobila koji će biti prikazan
+
+    useEffect(() => {
+        if (searchTerm === '') {
+            setDisplayCars(cars);  
+        } else {
+            setDisplayCars(cars.filter(car => 
+                car.model.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                car.brand.name.toLowerCase().includes(searchTerm.toLowerCase())
+            ));  
+        }
+    }, [searchTerm, cars]);
+
     return (
         <div className="ponuda-container">
-            {cars.map(car => <CarCard key={car.id} car={car} />)}
+            <div className="search-container">
+                <input 
+                    type="text" 
+                    placeholder="Search by car model..." 
+                    value={searchTerm} 
+                    onChange={e => setSearchTerm(e.target.value)}
+                />
+            </div>
+            {displayCars.map(car => <CarCard key={car.id} car={car} />)}
         </div>
     );
 }
