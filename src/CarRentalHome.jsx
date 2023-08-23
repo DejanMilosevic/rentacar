ccc4c00843da00d22c0ff6741f3f8033344fabdb
DejanMilosevic <milosevicdejan65@gmail.com>
2023-08-23 23:00:38 +0200

    dodat carousel sa slikama sa javnog apija

--- src/CarRentalHome.jsx
+++ src/CarRentalHome.jsx
@@ -1,7 +1,41 @@
-import React from 'react';
- 
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Slider from "react-slick";
 
 const CarRentalHome = () => {
+    const [images, setImages] = useState([]);
+    const ACCESS_KEY = 'AYoD1RB6ATmNpK69MpZK7lqW5fFyqTUtrHFhEZW7o1k';  
+    
+    useEffect(() => {
+      const fetchImages = async () => {
+        try {
+          const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: {
+              query: 'car',
+              per_page: 5,
+            },
+            headers: {
+              Authorization: `Client-ID ${ACCESS_KEY}`,
+            },
+          });
+  
+          setImages(response.data.results);
+        } catch (error) {
+          console.error("Greška prilikom dohvaćanja slika:", error);
+        }
+      };
+  
+      fetchImages();
+    }, []); 
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
+
     return (
         <div className="car-rental-home">
             <header className="rental-header">
@@ -21,6 +55,16 @@ const CarRentalHome = () => {
                     <h2>Excellent Service</h2>
                     <p>24/7 customer support for a seamless experience.</p>
                 </div>
+            </section>
+
+            <section className="image-carousel">
+                <Slider {...settings}>
+                    {images.map((image) => (
+                        <div key={image.id}>
+                            <img src={image.urls.regular} alt={image.alt_description || 'Automobil'} />
+                        </div>
+                    ))}
+                </Slider>
             </section>
         </div>
     );
