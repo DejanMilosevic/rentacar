7f8336e067571e311c9d4d28194041c4ef316eca
KrstinaPerovic <perovickrstina1@gmail.com>
2023-08-26 21:44:56 +0200

    omoguceno kreiranje rezervacija

--- rentacarback/app/Http/Controllers/RentalController.php
+++ rentacarback/app/Http/Controllers/RentalController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Car;
 use App\Models\Rental;
 use Illuminate\Http\Request;
 
@@ -15,16 +16,36 @@ class RentalController extends Controller
 
 public function store(Request $request)
 {
+    
     $validatedData = $request->validate([
         'car_id' => 'required|exists:cars,id',
         'user_id' => 'required|exists:users,id',
         'rent_start_date' => 'required|date',
         'rent_end_date' => 'required|date|after_or_equal:rent_start_date',
-        'total_price' => 'required|numeric'
+        'total_price' => 'numeric'
     ]);
 
+    // Pronađi automobil u bazi
+    $car = Car::find($validatedData['car_id']);
+    
+    // Proveri da li je automobil dostupan pre nego što ga rezervišete
+    if (!$car->is_available) {
+        return response()->json(['message' => 'Car is already reserved!'], 400);
+    }
+
+    // Postavi automobil kao nedostupan
+    $car->is_available = false;
+    $car->save();
+
+    // Kreiraj novo iznajmljivanje
     $rental = Rental::create($validatedData);
     return response()->json(['rental' => $rental, 'message' => 'Rental successfully added!'], 201);
 }
+ 
+
+
+
+ 
+ 
 
 }
--- src/CarCard.jsx
+++ src/CarCard.jsx
@@ -1,13 +1,56 @@
-import React from 'react';
- 
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const CarCard = ({ car }) => {
+    const [isAvailable, setIsAvailable] = useState(car.is_available);
+
+    const handleReservation = async () => {
+        // Čitanje podataka iz session storage
+        const user_id = sessionStorage.getItem('auth_id');
+        const authToken = sessionStorage.getItem('auth_token');
+
+        // Generisanje datuma od i do (na primjer, rezervacija od danas do sutra)
+        const today = new Date();
+        const tomorrow = new Date(today);
+        tomorrow.setDate(tomorrow.getDate() + 1);
+
+        // Formatiranje datuma u 'YYYY-MM-DD' formatu
+        const rent_start_date = today.toISOString().split('T')[0];
+        const rent_end_date = tomorrow.toISOString().split('T')[0];
+
+        // Izračunavanje ukupne cene (trenutno je uvek 1 dan)
+        const daysRented = (new Date(rent_end_date) - new Date(rent_start_date)) / (1000 * 60 * 60 * 24);
+        const total_price = daysRented * car.price_per_day;
+
+        try {
+            const response = await axios.post('http://127.0.0.1:8000/api/rentals', {
+                user_id: user_id,
+                car_id: car.id,
+                rent_start_date: rent_start_date,
+                rent_end_date: rent_end_date,
+                total_price: total_price
+            }, {
+                headers: {
+                    'Authorization': `Bearer ${authToken}`
+                }
+            });
+
+            if (response.status === 200 || response.status === 201) {
+                // Ako je uspešno sačuvano, ažuriraj status automobila
+                setIsAvailable(false);
+            }
+        } catch (error) {
+            console.error('Error during reservation:', error);
+        }
+    };
+
     return (
         <div className="car-card">
             <h2>{car.brand.name} - {car.model}</h2>
             <p><strong>Year:</strong> {car.year}</p>
             <p><strong>Price per day:</strong> ${car.price_per_day}</p>
-            <p><strong>Availability:</strong> {car.is_available ? "Available" : "Not Available"}</p>
+            <p><strong>Availability:</strong> {isAvailable ? "Available" : "Not Available"}</p>
+            {isAvailable && <button onClick={handleReservation}>Rezerviši</button>}
         </div>
     );
 }
